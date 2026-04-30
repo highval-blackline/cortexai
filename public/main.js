ab1cc3a6825c62766fbd124cd3ff92d4c7125e32
highval-blackline <yandirkardes@gmail.com>
2026-04-30 23:03:53 +0300

    UI: Reverted Google button shape and fixed mobile stretching

--- public/main.js
+++ public/main.js
@@ -181,7 +181,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 });
                 google.accounts.id.renderButton(
                     document.getElementById("googleButtonContainer"),
-                    { theme: "filled_black", size: "large", type: "standard", width: "280", shape: "pill" }
+                    { theme: "filled_black", size: "large", type: "standard", width: "250" }
                 );
                 google.accounts.id.prompt(); // Sağ üstteki popup'ı SADECE bunlara çıkar
             }
